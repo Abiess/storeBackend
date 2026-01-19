59edffc3b4973e2fff79efd4d052093b71857fda
Es-Soudati, Abdellah <Abdellah.Es-Soudati@DATEV.DE>
2026-01-19 12:28:50 +0100

    hdhd

--- src/main/java/storebackend/config/JacksonConfig.java
+++ src/main/java/storebackend/config/JacksonConfig.java
@@ -1,7 +1,7 @@
 package storebackend.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
@@ -17,16 +17,15 @@ public class JacksonConfig {
     public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
 
-        // Hibernate5Module registrieren - verhindert Lazy Loading Fehler
-        Hibernate5Module hibernate5Module = new Hibernate5Module();
+        // Hibernate6Module registrieren - verhindert Lazy Loading Fehler
+        Hibernate6Module hibernate6Module = new Hibernate6Module();
 
         // FORCE_LAZY_LOADING = false: Lazy-loaded Properties werden als null serialisiert
         // Das ist sicherer und vermeidet N+1 Query-Probleme
-        hibernate5Module.disable(Hibernate5Module.Feature.FORCE_LAZY_LOADING);
+        hibernate6Module.disable(Hibernate6Module.Feature.FORCE_LAZY_LOADING);
 
-        builder.modulesToInstall(hibernate5Module);
+        builder.modulesToInstall(hibernate6Module);
 
         return builder;
     }
 }
-
