30eb89e3e8362018d6cd876ea34fc6c95abb25ec
Es-Soudati, Abdellah <Abdellah.Es-Soudati@DATEV.DE>
2026-02-10 14:53:31 +0100

    12

--- src/main/java/storebackend/repository/CartItemRepository.java
+++ src/main/java/storebackend/repository/CartItemRepository.java
@@ -1,6 +1,8 @@
 package storebackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import storebackend.entity.CartItem;
 
@@ -9,7 +11,14 @@ import java.util.Optional;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    List<CartItem> findByCartId(Long cartId);
+
+    @Query("SELECT ci FROM CartItem ci " +
+           "LEFT JOIN FETCH ci.product p " +
+           "LEFT JOIN FETCH ci.variant v " +
+           "LEFT JOIN FETCH v.product " +
+           "WHERE ci.cart.id = :cartId")
+    List<CartItem> findByCartId(@Param("cartId") Long cartId);
+
     Optional<CartItem> findByCartIdAndVariantId(Long cartId, Long variantId);
     Optional<CartItem> findByCartIdAndProductIdAndVariantIsNull(Long cartId, Long productId);
     void deleteByCartId(Long cartId);
