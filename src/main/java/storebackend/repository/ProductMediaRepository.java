ccc377687c4b940d0a8d35bd53741548841b84d5
Es-Soudati, Abdellah <Abdellah.Es-Soudati@DATEV.DE>
2026-02-10 12:42:57 +0100

    Fix: Change hibernate ddl-auto to update for production

--- src/main/java/storebackend/repository/ProductMediaRepository.java
+++ src/main/java/storebackend/repository/ProductMediaRepository.java
@@ -1,6 +1,8 @@
 package storebackend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import storebackend.entity.ProductMedia;
 
@@ -9,8 +11,12 @@ import java.util.Optional;
 
 @Repository
 public interface ProductMediaRepository extends JpaRepository<ProductMedia, Long> {
-    List<ProductMedia> findByProductIdOrderBySortOrderAsc(Long productId);
-    Optional<ProductMedia> findByProductIdAndIsPrimaryTrue(Long productId);
+
+    @Query("SELECT pm FROM ProductMedia pm JOIN FETCH pm.media WHERE pm.product.id = :productId ORDER BY pm.sortOrder ASC")
+    List<ProductMedia> findByProductIdOrderBySortOrderAsc(@Param("productId") Long productId);
+
+    @Query("SELECT pm FROM ProductMedia pm JOIN FETCH pm.media WHERE pm.product.id = :productId AND pm.isPrimary = true")
+    Optional<ProductMedia> findByProductIdAndIsPrimaryTrue(@Param("productId") Long productId);
+
     void deleteByProductId(Long productId);
 }
-
